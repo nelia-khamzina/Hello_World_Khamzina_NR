8805a66ef5c7c70cef65cfea51cbf6d6855ece13
Неля Хамзина <hamzina.nr@edu.spbstu.ru>
2026-06-08 20:45:13 +0300

    Update task_6_0-3.py

--- project_6/task_6_0-3.py
+++ project_6/task_6_0-3.py
@@ -3,6 +3,6 @@ df = pd.read_csv('wild_boars.csv')
 column_names = df.columns.values
 for i in column_names[2:]:
     median_param = df[i].median()
-    with open("file_6_0-2_median_cabanchick.txt", "a") as file:
+    with open("file_6_0-3_median_cabanchick.txt", "a") as file:
         file.write(f"Boars median {i} is {median_param:.2f}\n")
     file.close()
