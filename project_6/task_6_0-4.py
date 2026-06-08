2aa83780feebca6e972eb79c07202b4f3489881e
Неля Хамзина <hamzina.nr@edu.spbstu.ru>
2026-06-08 20:44:36 +0300

    Update task_6_0-4.py

--- project_6/task_6_0-4.py
+++ project_6/task_6_0-4.py
@@ -3,6 +3,6 @@ df = pd.read_csv('wild_boars.csv')
 column_names = df.columns.values
 for i in column_names[2:]:
     mode_param = df[i].mode().values
-    with open("file_6_0-2_mode_cabanchick.txt", "a") as file:
+    with open("file_6_0-4_mode_cabanchick.txt", "a") as file:
         file.write(f"Boars mode {i} is {mode_param}\n")
     file.close()
